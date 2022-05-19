f694255621b9aa9a7f24696fbff50f7653638245
alexmeirelles <alemeirelles@msn.com>
2022-05-19 10:38:05 -0300

    fix: Module Names

--- Pizza Fresh Server/src/app.controller.ts
+++ Pizza Fresh Server/src/app.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
 
-
 @ApiTags('status')
 @Controller()
 export class AppController {
